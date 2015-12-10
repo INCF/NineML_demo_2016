01997f773a1157c10b6d30a4dd29a5dd19f9bbf9
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-12-10 13:36:35 +0100

    Update to four-panel figure script

--- four_panel_figure.py
+++ four_panel_figure.py
@@ -7,6 +7,8 @@ from __future__ import division
 import sys
 import argparse
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import neo
@@ -15,13 +17,16 @@ from quantities import ms, dimensionless
 
 def instantaneous_firing_rate(segment, begin, end):
     """Computed in bins of 0.1 ms """
-    bins = np.arange(begin, end, 0.1)
+    bin_width = 0.1
+    bins = np.arange(begin, end, bin_width)
     hist, _ = np.histogram(segment.spiketrains[0].time_slice(begin*ms, end*ms)/ms, bins)
     for st in segment.spiketrains[1:]:
         h, _ = np.histogram(st.time_slice(begin*ms, end*ms)/ms, bins)
         hist += h
-    return neo.AnalogSignal(hist, sampling_period=0.1*ms, units=dimensionless,
-                            channel_index=0, t_start=begin*ms, name="Spike count")
+    hist *= 1000/bin_width/len(segment.spiketrains)
+    return neo.AnalogSignal(hist, sampling_period=bin_width*ms, units=dimensionless,
+                            channel_index=0, t_start=begin*ms, name="Instantaneous firing rate")
+
 
 def mean_firing_rate(segment):
     duration = segment.spiketrains[0].t_stop - segment.spiketrains[0].t_start
@@ -30,13 +35,13 @@ def mean_firing_rate(segment):
     return n/len(segment.spiketrains)/duration.rescale('s')
 
 
-def plot_case(gs, data, t_start=900.0, t_stop=1200.0, title=""):
+def plot_case(gs, data, t_start=900.0, t_stop=1200.0, title="", yticks=None):
     global j
 
     gs1 = gridspec.GridSpecFromSubplotSpec(2, 1,
                                            subplot_spec=gs[j//2, j%2],
                                            height_ratios=[4, 1],
-                                           hspace=0.2)
+                                           hspace=0.3)
     ax1 = plt.subplot(gs1[0])
     ax2 = plt.subplot(gs1[1])
 
@@ -55,13 +60,16 @@ def plot_case(gs, data, t_start=900.0, t_stop=1200.0, title=""):
 
     ifr = instantaneous_firing_rate(data, t_start, t_stop)
     t = ifr.times.rescale(ms)
-    ax2.plot(t, ifr, color='0.5')
-    ax2.fill_between(t, 0, ifr, color='0.5')
+    ax2.plot(t, ifr, color='0.5', lw=0.001, solid_joinstyle='miter')
+    ax2.fill_between(t, 0, ifr, color='0.5', lw=0)
 
-    start, stop = ax2.get_ylim()
-    stop50 = (stop//50 + 1) * 50
-    yres = stop50/5
-    ticks = np.arange(0, stop + yres, yres)
+    if yticks:
+        ticks = yticks
+    else:
+	start, stop = ax2.get_ylim()
+        stop50 = (stop//50 + 1) * 50
+        yres = stop50/5
+        ticks = np.arange(0, stop + yres, yres)
     ax2.set_yticks(ticks)
 
     for ax in (ax1, ax2):
@@ -75,7 +83,7 @@ def plot_case(gs, data, t_start=900.0, t_stop=1200.0, title=""):
     if j//2 == 1:
         ax2.set_xlabel("Time [ms]")
     if j%2 == 0:
-        ax2.set_ylabel("# spikes")
+        ax2.set_ylabel("Firing rate [/s]")
 
     plt.show(block=False)
     fig.canvas.draw()
@@ -93,10 +101,12 @@ config = parser.parse_args()
 
 
 plt.rcParams.update({
-    'lines.linewidth': 0.5,
-    'legend.fontsize': 'small',
-    'axes.titlesize': 'large',
-    'axes.linewidth': 0.4,
+    'lines.linewidth': 0.01,
+    'xtick.labelsize': 'x-small',
+    'ytick.labelsize': 'x-small',
+    'legend.fontsize': 'x-small',
+    'axes.titlesize': 'medium',
+    'axes.linewidth': 0.01,
     'font.size': 8,
     'savefig.dpi': 200,
     'interactive': False
@@ -110,18 +120,26 @@ j = 0
 
 
 titles = {
-    "SR": "A: almost synchronized",
-    "SIfast": "B: fast oscillation",
-    "AI": "C: stationary",
-    "SIslow": "D: slow oscillation"
+    "SR": "A: Synchronous Regular",
+    "SIfast": "B: Synchronous Irregular (fast)",
+    "AI": "C: Asynchronous Irregular",
+    "SIslow": "D: Synchronous Irregular (slow)"
 }
 
 time_range = {
     "SR": (500, 600),
-    "SIfast": (900, 1200),
-    "AI": (900, 1200),
-    "SIslow": (900, 1200)
+    "SIfast": (1000, 1200),
+    "AI": (1000, 1200),
+    "SIslow": (1000, 1200)
 }
+
+firing_rate_ticks = {
+    "SR": (0, 1000, 2000, 3000, 4000, 5000, 6000),
+    "SIfast": (0, 100, 200, 300, 400),
+    "AI": (0, 50, 100, 150, 200),
+    "SIslow": (0, 50, 100, 150, 200, 250)
+}
+
 
 def get_case(filename):
     for case in ("SR", "SIfast", "AI", "SIslow"):
@@ -135,7 +153,7 @@ for data_file in config.datafiles:
     data = io.read()[0].segments[0]
     case = get_case(data_file)
     t_start, t_stop = time_range[case]
-    plot_case(gs, data, t_start, t_stop, titles[case])
+    plot_case(gs, data, t_start, t_stop, titles[case], yticks=firing_rate_ticks[case])
 
 
 fig.savefig(config.output)
